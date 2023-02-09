af2746e36d927865c1a6dfe6f4e91cf7e3a06676
devmikh <mishacyb@gmail.com>
2023-02-09 11:51:13 -0800

    Socket server setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,41 @@
-import { useState } from 'react'
+import io from 'socket.io-client';
+const socket = io.connect("http://localhost:3000");
+
+import { useEffect, useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [gameData, setGameData] = useState({
+    player: {
+      match_stats: {
+        kills: 0,
+        deaths: 0
+      },
+      weapons: {
+        weapon_1: {
+          ammo_clip: 0,
+          ammo_reserve: 0
+        },
+        weapon_2: {
+          ammo_clip: 0,
+          ammo_reserve: 0
+        }
+      }
+    }
+  });
+
+  useEffect(() => {
+    socket.on("game_data", (data) => {
+      console.log(data);
+      setGameData(data);
+    });
+  }, [socket]);
 
   return (
     <div>
-      <button onClick={() => setCount((count) => count + 1)}>
-        count is {count}
-      </button>
+      <h1>Kills: {gameData.player.match_stats.kills || 0}</h1>
+      <h1>Deaths: {gameData.player.match_stats.deaths || 0}</h1>
+      <h1>Bullets: {gameData.player.weapons.weapon_2.ammo_clip || 0} / {gameData.player.weapons.weapon_2.ammo_reserve || 0}</h1>
     </div>
   )
 }
--- server.js
+++ server.js
@@ -1,19 +1,53 @@
-const express = require('express');
-
+const express = require("express");
 const app = express();
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require('cors');
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-app.get("/", (req, res, next) => {
-  res.send("Hello");
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"]
+  }
+})
+
+
+
+// io.on('connection', (socket) => { 
+//   console.log(`User connected: ${socket.id}`);
+
+//   socket.on("send_message", (data) => {
+//     socket.broadcast.emit("receive_message", data);
+//   });
+// });
+
+app.post("/", (req, res) => {
+  console.log(req.body);
+  io.emit("game_data", req.body);
+  res.status(200);
+  res.end();
 });
 
-app.post("/", (req, res, next) => {
-  console.log("request", req.body);
-  res.send("ok");
-});
+server.listen(3000, () => {
+  console.log("Server is running on port 3000");
+})
 
-app.listen(3000, () => {
-  console.log("Server listening on port 3000");
-});
+
+
+
+// server.listen(3000, () => {
+//   console.log("Server is listening on port 3000");
+// });
+
+
+
+
+
+
+
