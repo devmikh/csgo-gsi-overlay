1d00f11b99e361fe13f1b5a506465a219b417ddd
devmikh <mishacyb@gmail.com>
2023-02-10 18:31:07 -0800

    Fetched player stats and pass to Stats component for rendering

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,43 +1,39 @@
 import io from 'socket.io-client';
-const socket = io.connect("http://localhost:3000");
+const socket = io.connect('http://localhost:3000');
 
 import { useEffect, useState } from 'react';
 
+import Stats from './components/Stats';
+
 function App() {
 
-  const [gameData, setGameData] = useState({
+  const [data, setData] = useState({
     player: {
+      name: '',
+      state: {
+        health: 100,
+      },
       match_stats: {
         kills: 0,
+        assists: 0,
         deaths: 0
       },
-      weapons: {
-        weapon_1: {
-          ammo_clip: 0,
-          ammo_reserve: 0
-        },
-        weapon_2: {
-          ammo_clip: 0,
-          ammo_reserve: 0
-        }
-      }
     }
   });
 
   useEffect(() => {
-    socket.on("game_data", (data) => {
+    socket.on('data', (data) => {
       console.log(data);
-      setGameData(data);
+      setData(data);
     });
   }, [socket]);
 
   return (
     <div>
-      <h1>Kills: {gameData.player.match_stats.kills || 0}</h1>
-      <h1>Deaths: {gameData.player.match_stats.deaths || 0}</h1>
-      <h1>Bullets: {gameData.player.weapons.weapon_2.ammo_clip || 0} / {gameData.player.weapons.weapon_2.ammo_reserve || 0}</h1>
+      <Stats playerStats={ data.player.match_stats } />
+      <p>{ data.player.name }</p>
     </div>
   )
 }
 
-export default App
+export default App;
