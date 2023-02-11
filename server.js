b98598871c07308486cc31577b42eb1354865f84
devmikh <mishacyb@gmail.com>
2023-02-10 18:29:41 -0800

    Code formatting

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const http = require("http");
-const { Server } = require("socket.io");
+const http = require('http');
+const { Server } = require('socket.io');
 const cors = require('cors');
 
 app.use(cors());
@@ -12,38 +12,21 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
-    methods: ["GET", "POST"]
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST']
   }
 })
 
-
-
-// io.on('connection', (socket) => { 
-//   console.log(`User connected: ${socket.id}`);
-
-//   socket.on("send_message", (data) => {
-//     socket.broadcast.emit("receive_message", data);
-//   });
-// });
-
-app.post("/", (req, res) => {
+app.post('/', (req, res) => {
   console.log(req.body);
-  io.emit("game_data", req.body);
+  io.emit('data', req.body);
   res.status(200);
   res.end();
 });
 
 server.listen(3000, () => {
-  console.log("Server is running on port 3000");
+  console.log('Server is running on port 3000');
 })
-
-
-
-
-// server.listen(3000, () => {
-//   console.log("Server is listening on port 3000");
-// });
 
 
 
